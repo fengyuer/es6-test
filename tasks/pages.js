f359219a8b35741005a3543fff55f8ed7cd8c4cd
pactera.yufeng.zhang1 <yufeng.zhang1@pactera.com>
2017-09-04 14:30:01 +0800

    创建模板脚本

---
+++ tasks/pages.js
@@ -0,0 +1,10 @@
+import gulp from 'gulp'
+import gulpif from 'gulp-if'
+import livereload from 'gulp-livereload'
+import args from './util/args'
+
+gulp.task('pages', () => {
+    return gulp.src('app/**/*.ejs')     // app下的所有ejs文件
+        .pipe(gulp.dest('server'))      // 把app文件原封不动的copy到server目录下
+        .pipe(gulpif(args.watch,livereload()))  //监听热更新
+})
