4ab275b3e88f0e9870776fbefbb7767843e6838c
pactera.yufeng.zhang1 <yufeng.zhang1@pactera.com>
2017-09-22 17:48:26 +0800

    set-map

--- app/js/class/l10 Set-map.js
+++ app/js/class/l10 Set-map.js
@@ -104,8 +104,62 @@
             console.log(item)
             
         }, this);
-
-        
-
     }
+}
+
+// weakSet
+{
+    // WeakSet 结构与 Set 类似，也是不重复的值的集合。
+    // 但是，它与 Set 有两个区别。
+    // 首先，WeakSet 的成员只能是对象，而不能是其他类型的值。
+
+    // 其次，WeakSet 中的对象都是弱引用，
+    // 即垃圾回收机制不考虑 WeakSet 对该对象的引用，
+    // 也就是说，如果其他对象都不再引用该对象，那么垃圾回收机制会自动回收该对象所占用的内存，不考虑该对象还存在于 WeakSet 之中。
+    let weakList = new WeakSet();
+    let arg = {}
+    weakList.add(arg)
+    weakList.add([1])
+    console.log('weakSet',weakList)
+
+    // 无法遍历
+}
+
+// map
+{
+    let map = new Map();
+    let arr = ['123']
+    let obj = {p: 'hello word'}
+
+    // map使用set方法 添加键
+    map.set(arr,456)
+    map.set(obj,'789')
+    // map使用get方法 读取这个键
+    console.log('map---',map.get(arr),map)
+}
+{
+    // map定义的第二种方法
+    let map = new Map([['a','123'],['b','456']])
+
+    console.log('map arg---',map)
+    console.log(map.get('b'))
+    console.log('size---',map.size)
+    // 删除
+    console.log('delete---',map.delete('a'),map)
+    // 清空
+    console.log('clear---',map.clear(),map)
+
+    let m = new Map([['a',123],['b',456],['a','789']])
+    console.log('m---',m)
+}
+
+// WeakMap
+{
+    // 它的key一定是对象
+    let weakmap = new WeakMap();
+
+    let o = {}
+    weakmap.set(o,123)
+    console.log('weakmap---',weakmap)
+
 }
